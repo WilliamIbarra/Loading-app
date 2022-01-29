ad5859c9a25ce1d5de449d1e3ee86f7f0ba8a0a2
William Ibarra <wa_ibarra@hotmail.com>
2022-01-28 23:54:13 -0600

    [Added] Default text and color on thecustom view

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,10 +2,36 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
+
+
+private enum class Status(val status: Int) {
+    NORMAL(R.string.button_name),
+    DOWNLOAD(R.string.button_loading);
+
+    fun next() = when (this) {
+        NORMAL -> DOWNLOAD
+        DOWNLOAD -> NORMAL
+    }
+}
+
+private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    style = Paint.Style.FILL
+    textAlign = Paint.Align.LEFT
+    textSize = 55.0f
+    typeface = Typeface.create( "", Typeface.BOLD)
+}
+
+private var normalColor = 0
+private var downloadColor = 0
+
+private var normalText: String?  = null
+private var downloadText: String? = null
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -13,7 +39,21 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
+    private var status = Status.NORMAL         // The active selection.
+
     private val valueAnimator = ValueAnimator()
+
+
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        status = status.next()
+        contentDescription = resources.getString(status.status)
+
+        invalidate()
+        return true
+    }
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
@@ -21,12 +61,40 @@ class LoadingButton @JvmOverloads constructor(
 
 
     init {
+        isClickable = true
 
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            normalColor = getColor(R.styleable.LoadingButton_normal_color, 0)
+            downloadColor = getColor(R.styleable.LoadingButton_download_color, 0)
+            normalText = getString(R.styleable.LoadingButton_normal_text)
+            downloadText = getString(R.styleable.LoadingButton_download_text)
+        }
     }
 
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+        // set background
+
+        paint.color = when(status) {
+        Status.NORMAL -> normalColor
+            else -> downloadColor
+        }
+
+        val rect = Rect(0, 0, width, height)
+
+            canvas?.drawRect(rect, paint)
+
+            canvas?.save()
+
+            paint.color = Color.WHITE
+            paint.textAlign = Paint.Align.CENTER
+
+            canvas?.drawText(normalText ?: "Default", rect.exactCenterX(), rect.centerY().toFloat(), paint)
+
+            canvas?.restore()
+
 
     }
 
